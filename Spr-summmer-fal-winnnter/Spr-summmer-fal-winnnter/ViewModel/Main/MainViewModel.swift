d241d4346eb11677309a043fe485c103f2dac195
ghnn-n <cgh6470@gmail.com>
2025-05-23 22:58:52 +0900

    [Feat] #27 - 5일간 예보 구현

--- Spr-summmer-fal-winnnter/Spr-summmer-fal-winnnter/ViewModel/Main/MainViewModel.swift
+++ Spr-summmer-fal-winnnter/Spr-summmer-fal-winnnter/ViewModel/Main/MainViewModel.swift
@@ -21,14 +21,21 @@ class MainViewModel {
     struct Output {
         let showSettingMenu = PublishRelay<Void>()
         let mainCellData = BehaviorRelay<WeatherResponse?>(value: nil)
-        let forecastListCellData = BehaviorRelay<ForecastData?>(value: nil)
+        let tenForecastCellData = BehaviorRelay<TenForecastData?>(value: nil)
+        let allForecastCellData = BehaviorRelay<[AllForecastData]?>(value: nil)
     }
     
-    struct ForecastData {
+    struct TenForecastData {
         let forecastList: [ForecastList]
         let weatherIcons: [UIImage]
     }
     
+    struct AllForecastData {
+        let forecastList: CustomForecastList
+        let weatherIcons: UIImage
+    }
+    
+    private var customForecastDatas = [AllForecastData]()
     private let disposeBag = DisposeBag()
     
     let input = PublishRelay<Input>()
@@ -61,12 +68,105 @@ class MainViewModel {
                     image.append(changedData)
                 }
                 
+                self.transformForecastListData(data: weather.list)
+                
+                image = [UIImage](image.prefix(12))
                 var list = [ForecastList](weather.list.prefix(12))
+                
                 list.removeFirst(2)
                 image.removeFirst(2)
                 
-                let result = ForecastData(forecastList: list, weatherIcons: image)
-                self.output.forecastListCellData.accept(result)
+                let tenResult = TenForecastData(forecastList: list, weatherIcons: image)
+                self.output.tenForecastCellData.accept(tenResult)
+            } onFailure: { error in
+                print(error)
+            }.disposed(by: disposeBag)
+
+    }
+    
+    private func transformForecastListData(data: [ForecastList]) {
+        let firstHour = String(data[0].dtTxt.components(separatedBy: " ")[1].prefix(2))
+        var list = data
+        var box = [ForecastList]()
+        var result = [[ForecastList]]()
+        
+        switch firstHour {
+        case "00":
+            box.append(list.removeFirst())
+            fallthrough
+        case "03":
+            box.append(list.removeFirst())
+            fallthrough
+        case "06":
+            box.append(list.removeFirst())
+            fallthrough
+        case "09":
+            box.append(list.removeFirst())
+            fallthrough
+        case "12":
+            box.append(list.removeFirst())
+            fallthrough
+        case "15":
+            box.append(list.removeFirst())
+            fallthrough
+        case "18":
+            box.append(list.removeFirst())
+            fallthrough
+        case "21":
+            box.append(list.removeFirst())
+            fallthrough
+        default:
+            break
+        }
+        
+        result.append(box)
+        box.removeAll()
+        
+        while list.count > 1 {
+            box.append(list.removeFirst())
+            if box.count == 8 {
+                result.append(box)
+                box.removeAll()
+            }
+        }
+        
+        if box.count > 0 {
+            result.append(box)
+        }
+        
+        var customForecastList = [CustomForecastList]()
+        
+        result.forEach {
+            let day = String($0[0].dtTxt.split(separator: " ")[0].suffix(2))
+            let tempMin = $0.sorted(by: { $0.main.tempMin < $1.main.tempMin })[0].main.tempMin
+            let tempMax = $0.sorted(by: { $0.main.tempMax > $1.main.tempMax })[0].main.tempMin
+            let pop = $0.sorted(by: { $0.pop > $1.pop })[0].pop
+            let icon = $0.sorted(by: { $0.pop > $1.pop })[0].weather[0].icon
+            
+            customForecastList.append(CustomForecastList(day: day,
+                                                     tempMin: tempMin,
+                                                     tempMax: tempMax,
+                                                     pop: pop,
+                                                     icon: icon))
+        }
+        
+        customForecastList.forEach {
+            self.fetchCustomForecastListIcon(data: $0)
+        }
+        
+    }
+    
+    private func fetchCustomForecastListIcon(data: CustomForecastList) {
+        let customForecast = Single<CustomForecastList>.just(data)
+        let icon = NetworkManager.shared.fetchIconImageData(iconIds: data.icon)
+        
+        Single.zip(customForecast, icon)
+            .subscribe { custom, data in
+                guard let image = UIImage(data: data) else { return }
+                self.customForecastDatas.append(AllForecastData(forecastList: custom, weatherIcons: image))
+                if self.customForecastDatas.count == 5 {
+                    self.output.allForecastCellData.accept(self.customForecastDatas)
+                }
             } onFailure: { error in
                 print(error)
             }.disposed(by: disposeBag)
