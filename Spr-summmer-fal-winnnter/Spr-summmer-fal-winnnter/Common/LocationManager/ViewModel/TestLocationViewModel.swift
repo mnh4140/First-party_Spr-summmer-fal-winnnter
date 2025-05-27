9b2662e94bce5bad3fff1971e80dbd755e2c13bc
NH <mnh4140@gmail.com>
2025-05-27 13:39:58 +0900

    [Chore] LocationViewModel 주석

--- Spr-summmer-fal-winnnter/Spr-summmer-fal-winnnter/Common/LocationManager/ViewModel/TestLocationViewModel.swift
+++ Spr-summmer-fal-winnnter/Spr-summmer-fal-winnnter/Common/LocationManager/ViewModel/TestLocationViewModel.swift
@@ -9,18 +9,16 @@ import Foundation
 import RxSwift
 import RxRelay
 
-class ViewModel {
 class LocationViewModel {
     var disposeBag = DisposeBag()
-    let fetchAddressRelay = PublishRelay<[AddressData.Document]>() // 주소 -> 위도 경도 가져오기
     
     // 최신 상태(주소)를 UI에서 항상 보여줘야 하기 때문에
     // 마지막 값을 저장하고 방출할 수 있는 BehaviorRelay로 사용
-    let regionCodeRelay = BehaviorRelay<[RegionCodeResponse.Document]>(value: []) // 위도 경도 -> 주소로 가져오기
+    let fetchAddressRelay = PublishRelay<[AddressData.Document]>() // 주소 정보 방출
+    let regionCodeRelay = BehaviorRelay<[RegionCodeResponse.Document]>(value: []) // 위도 경도 좌표 정보 방출
     
-    /// - 주소를 위도 경도 값으로 변환
+    /// - 주소를 위도 경도 값으로 변환하는 API 호출
     /// - query 값은 검색어
-    /// - 현재는 주소 검색 결과만 받아오고, 위도 경도 가져오는 로직은 없음
     func fetchAddress(query: String) {
         var components = URLComponents(string: "https://dapi.kakao.com/v2/local/search/address")
         components?.queryItems = [
